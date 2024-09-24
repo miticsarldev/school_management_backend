4c0503d35af0528d4d6917af074538322ce4a377
sounkalo-mitic <sounkalo.sidibe@miticsarl.com>
2024-09-24 17:24:26 +0200

    [ESM004]: creation(interface,model,controller,routes)

---
+++ src/models/attendance.model.ts
@@ -0,0 +1,44 @@
+import { Schema, model } from 'mongoose';
+import { IAttendance } from '../types/model.attendance.type';
+
+
+// Schéma Mongoose pour Attendance
+const attendanceSchema = new Schema<IAttendance>(
+  {
+    student_id: {
+      type: Schema.Types.ObjectId,
+      ref: 'User', // Référence au modèle User pour les étudiants
+      required: false,
+    },
+    teacher_id: {
+      type: Schema.Types.ObjectId,
+      ref: 'User', // Référence au modèle User pour les professeurs
+      required: false,
+    },
+    timetable_id: {
+      type: Schema.Types.ObjectId,
+      ref: 'TimeTable', // Référence au modèle TimeTable pour le cours
+      required: true,
+    },
+    status: {
+      type: Boolean,
+      required: true, // Présence obligatoire
+    },
+    desc: {
+      type: String,
+      required: false, // Optionnel
+    },
+    date: {
+      type: Date,
+      default: Date.now, // Date par défaut: la date actuelle
+    },
+  },
+  {
+    timestamps: true, // Ajoute createdAt et updatedAt
+  }
+);
+
+// 3. Modèle Mongoose pour Attendance
+const Attendance = model<IAttendance>('Attendance', attendanceSchema);
+
+export default Attendance;
