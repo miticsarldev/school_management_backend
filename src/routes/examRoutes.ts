d980390d3cbc7ed7870ad379905d75c788b9c410
sounkalo-mitic <sounkalo.sidibe@miticsarl.com>
2024-09-25 17:05:24 +0200

    [ESM-005] : "creation des routes"

--- src/routes/examRoutes.ts
+++ src/routes/examRoutes.ts
@@ -1,11 +1,16 @@
-// routes/exam.routes.ts
 import express from 'express';
-import { addExam, updateExam, deleteExam } from '../controllers/exam.controller';
+import { addExam, getExams, getExamById, updateExam, deleteExam } from '../controllers/exam.controller';
 
 const router = express.Router();
 
 // Route pour ajouter un examen
 router.post('/', addExam);
+
+// Route pour récupérer tous les examens
+router.get('/', getExams);
+
+// Route pour récupérer un examen par ID
+router.get('/:id', getExamById);
 
 // Route pour modifier un examen
 router.put('/:id', updateExam);
--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,16 @@
 import mongoose, { ConnectOptions } from "mongoose";
 import exam_typeRoutes from "./routes/exam_typeRoutes";
+import examRoutes from "./routes/examRoutes";
 import { config } from "dotenv";
 config();
 
 import app from "./app";
 
+//routes pour exam_type
 app.use('/api',exam_typeRoutes)
+
+//routes pour exam 
+app.use('/api/exams', examRoutes);
 
 const PORT = process.env.PORT || 4444;
 
