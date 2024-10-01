538ed20de7b73904c7a42bee8f13c961cfe55e5f
Imran-Mitic <imran.toure@miticsarl.com>
2024-10-01 15:39:20 +0000

    Ajout des calculs

--- src/models/Payroll.model.ts
+++ src/models/Payroll.model.ts
@@ -21,4 +21,24 @@ const PayrollSchema = new Schema<IPayroll>({
   },
 }, { timestamps: true }); // Ajoute les champs `createdAt` et `updatedAt`
 
+  // Méthode pour calculer le salaire restant
+  PayrollSchema.methods.calculerSalaireRestant = function() {
+    return this.salaire_total - this.montant_paye;
+  };
+  // Méthode pour ajouter un paiement
+PayrollSchema.methods.ajouterPaiement = function (montant: number) {
+  // Ajoute le montant au champ montant existant
+  this.montant += montant;
+
+  // Si le montant total est atteint ou dépassé, le statut est mis à jour
+  if (this.montant >= this.salaire_total) {
+    this.montant = this.salaire_total; // Assure que le montant payé ne dépasse pas le salaire total
+    this.statut = true; // Met à jour le statut pour indiquer que tout est payé
+  }
+
+  // Met à jour la date de paiement
+  this.updatedAt = new Date();
+  return this.save(); // Sauvegarde les changements dans la base de données
+};
+
 export const Payroll = model<IPayroll>("Payroll", PayrollSchema);
