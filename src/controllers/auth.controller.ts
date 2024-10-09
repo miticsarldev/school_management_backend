0f879da0eeefb8514dfa03adb3ae0d51b195a823
Aboubacar SANGARE <abouba.sang@outlook.com>
2024-10-09 21:40:01 +0000

    fix 'adding parent' into role type

--- src/controllers/auth.controller.ts
+++ src/controllers/auth.controller.ts
@@ -204,6 +204,7 @@ export const login = async (req: Request, res: Response) => {
     res.cookie("refreshToken", refreshToken, { httpOnly: true, secure: true });
     res.json({ user: user, accessToken });
   } catch (error) {
+    console.log(error);
     res.status(500).json({ message: "Server error" });
   }
 };
